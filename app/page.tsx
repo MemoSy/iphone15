3771e29a887f6d47872dc4de38aa677e8c577d60
MemoSy <ahmeddalhalabi1@gmail.com>
2024-11-15 11:47:50 +0300

    finish 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
-import Features from "@/components/Features";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import Highlights from "@/components/Highlights";
-import HowItWorks from "@/components/HowItWorks";
-import Model from "@/components/Model";
-
-
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+
+// import Features from "@/components/Features";
+// import Hero from "@/components/Hero";
+// import Highlights from "@/components/Highlights";
+// import HowItWorks from "@/components/HowItWorks";
+// import Model from "@/components/Model";
+
+const Features = dynamic(() => import("@/components/Features"), { ssr: false });
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const Highlights = dynamic(() => import("@/components/Highlights"), { ssr: false });
+const HowItWorks = dynamic(() => import("@/components/HowItWorks"), { ssr: false });
+const Model = dynamic(() => import("@/components/Model"), { ssr: false });
+
+
+
 
 export default function Home() {
   return (
